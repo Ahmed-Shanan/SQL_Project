4a126ac8241f6b4b0bfb6d391c9826e5cbaa1ece
Ahmed <ahmed.g.shanan@gmail.com>
2026-08-15 10:00:26 +0400

    new file

---
+++ WITH avg_salaries AS (.sql
@@ -0,0 +1,55 @@
+WITH avg_salaries AS (
+    SELECT
+    job_country,
+    AVG(salary_year_avg) AS avg_salary
+    FROM job_postings_fact
+    group BY job_country
+)
+
+SELECT 
+job_postings_fact.job_id,
+job_postings_fact.job_title,
+company_dim.name,
+job_postings_fact.salary_year_avg AS salary_rate,
+CASE 
+    WHEN job_postings_fact.salary_year_avg > avg_salaries.avg_salary 
+    THEN 'Above Average'
+    ELSE 'Below Average'
+END AS salary_comparison,
+EXTRACT(MONTH FROM job_postings_fact.job_posted_date) AS posted_month
+FROM job_postings_fact
+INNER JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+INNER JOIN avg_salaries ON job_postings_fact.job_country = avg_salaries.job_country
+ORDER BY  posted_month DESC
+
+
+WITH avg_salaries AS (
+    SELECT 
+        job_country, 
+        AVG(salary_year_avg) AS avg_salary
+    FROM job_postings_fact
+    GROUP BY job_country
+)
+SELECT
+    -- Gets basic job info
+    job_postings.job_id,
+    job_postings.job_title,
+    companies.name AS company_name,
+    job_postings.salary_year_avg AS salary_rate,
+    -- categorizes the salary as above or below average the average salary for the country
+    CASE
+        WHEN job_postings.salary_year_avg > avg_salaries.avg_salary
+        THEN 'Above Average'
+        ELSE 'Below Average'
+    END AS salary_category,
+    -- gets the month and year of the job posting date
+    EXTRACT(MONTH FROM job_postings.job_posted_date) AS posting_month
+FROM
+    job_postings_fact as job_postings
+INNER JOIN
+    company_dim as companies ON job_postings.company_id = companies.company_id
+INNER JOIN
+    avg_salaries ON job_postings.job_country = avg_salaries.job_country
+ORDER BY
+    -- Sorts it by the most recent job postings
+    posting_month desc
